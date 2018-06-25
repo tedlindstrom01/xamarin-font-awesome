791fb499b3245ee25bc13b9f520921eb73cf70d6
tlindstrom <tedlin01@gmail.com>
2018-06-25 02:54:08 +0200

    FontAwesome

---
+++ XamarinFontAwesome/XamarinFontAwesome/FontAwesome.xaml.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace XamarinFontAwesome
+{
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class FontAwesome : Label
+	{
+		public FontAwesome ()
+		{
+			InitializeComponent ();
+
+			if (Device.RuntimePlatform == Device.Android)
+			{
+				Resources.Add("FontAwesome-Regular", "FontAwesomeRegular.ttf#Font Awesome 5 Free");
+				Resources.Add("FontAwesome-Solid", "FontAwesomeSolid.ttf#Font Awesome 5 Free");
+			}
+			else
+			{
+				Resources.Add("FontAwesome-Regular", "FontAwesome5FreeRegular");
+				Resources.Add("FontAwesome-Solid", "FontAwesome5FreeSolid");
+			}
+		}
+	}
+}
